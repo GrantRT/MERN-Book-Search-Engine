1dac23dab8725987cca304e6b3df91ec816ef5f1
GrantRT <granto_92@hotmail.com>
2022-05-24 15:48:00 +0100

    refactored server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
-// Bring in Apollo Server 
-const { Apollo Server } require ('apollo-server-express')
+// const routes = require('./routes');
+
+// Bring in Apollo Server, typeDefs, resolvers and authMiddleware
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// Implement ApolloServer
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+// Apply ApolloServer to Express server as middleware
+// server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,8 +28,27 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// routes not needed? React app will handle the routing
+// app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// Setting up a wildcard route that serves the front end when a request for a non-API route is receieved.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+// Creating apollo server with the graphql schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  // Apply ApolloServer to Express server as middleware
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+};
+
+// Calling the async func to start the server
+startApolloServer(typeDefs, resolvers);
