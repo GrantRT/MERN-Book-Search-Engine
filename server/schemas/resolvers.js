72cd6b480b11e4a8b97d17b598cd3efb944afb51
GrantRT <granto_92@hotmail.com>
2022-05-24 18:08:51 +0100

    started resolver

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,45 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    // Get single user by the logged in users id
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+
+        return userData;
+      }
+      throw new AuthenticationError('Please log in');
+    },
+  },
+  Mutation: {
+    // Create a user
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    // Login a user
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Can't find this user");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Wrong Password!');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    // TODO: Save a book to a users 'savedBooks'
+  },
+};
