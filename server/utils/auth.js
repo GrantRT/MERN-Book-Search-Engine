c244735028c89bc18ff55fe1a3ed87d8cf809eb3
GrantRT <granto_92@hotmail.com>
2022-05-27 16:01:56 +0100

    debugging

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -23,9 +23,8 @@ module.exports = {
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+    } catch (err) {
+      console.log(err + 'Invalid token');
     }
 
     return req;
